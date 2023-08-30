e552c5d3cfdbb944bc6fe1f6f956779b208ed275
Mohammadhossein <siahkamarii1363@gmail.com>
2023-08-30 18:05:20 +0200

    master

--- src/Containers/Body/Body.jsx
+++ src/Containers/Body/Body.jsx
@@ -6,45 +6,49 @@ import axios from 'axios';
 const Body = () => {
 
 
-    const [responseDataApi,setResponseDataApi] = useState([])
+    const [responseDataApi, setResponseDataApi] = useState([])
 
-//     const express = require('express');
-// const cors = require('cors');
+    //     const express = require('express');
+    // const cors = require('cors');
 
-// const app = express();
+    // const app = express();
 
-// // افزودن cors به عنوان middleware
-// app.use(cors());
+    // // افزودن cors به عنوان middleware
+    // app.use(cors());
 
-// // تعریف مسیرها و دیگر تنظیمات سرور
-// // ...
+    // // تعریف مسیرها و دیگر تنظیمات سرور
+    // // ...
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+    // const PORT = process.env.PORT || 3000;
+    // app.listen(PORT, () => {
+    //   console.log(`Server is running on port ${PORT}`);
+    // });
 
 
     useEffect(() => {
         // Define an async function to make the request
         async function fetchData() {
-          try {
-            // Send the GET request and await the response
-            const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en' );
-            
-            console.log('دریافتی از سرور:', response.data);
-            setResponseDataApi(response.data);
-          } catch (error) {
-            console.error('خطا در درخواست:', error);
-          }
+            try {
+                // Send the GET request and await the response
+                const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en');
+
+                console.log('دریافتی از سرور:', response.data);
+                setResponseDataApi(response.data);
+            } catch (error) {
+                console.error('خطا در درخواست:', error);
+            }
         }
-      
+
         // Call the async function
         fetchData();
-      }, []);
+    }, []);
 
     return (
         <div className='body-main'>
+            <div className="titr-container">
+                <h2 className="titr">Online list of cryptocurrency prices</h2>
+            </div>
+            <div className="table-container">
             <table className='table1'>
                 <thead>
                     <tr>
@@ -57,20 +61,21 @@ const Body = () => {
                     </tr>
                 </thead>
                 <tbody>
-        {responseDataApi.map(item => (
-          <tr key={item.id}>
-            <td>{item.market_cap_rank}</td>
-            <td>{item.name}</td>
-            <td>{item.symbol}</td>
-            <td>{item.current_price}</td>
-            <td>{item.market_cap}</td>
-            <td>{item.market_cap_change_percentage_24h}</td>
-            
-          </tr>
-        ))}
-      </tbody>
+                    {responseDataApi.map(item => (
+                        <tr key={item.id}>
+                            <td>{item.market_cap_rank}</td>
+                            <td>{item.name}</td>
+                            <td>{item.symbol}</td>
+                            <td>{item.current_price}</td>
+                            <td>{item.market_cap}</td>
+                            <td>{item.market_cap_change_percentage_24h}</td>
+
+                        </tr>
+                    ))}
+                </tbody>
             </table>
 
+            </div>
         </div>
     );
 }
