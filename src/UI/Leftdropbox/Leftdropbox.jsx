f6b70728332c57000b80e5c770cd3becc6033d75
Mohammadhossein <siahkamarii1363@gmail.com>
2023-09-02 16:13:25 +0200

    master

--- src/UI/Leftdropbox/Leftdropbox.jsx
+++ src/UI/Leftdropbox/Leftdropbox.jsx
@@ -1,7 +1,8 @@
 import './Leftdropbox.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useContext } from 'react';
 import UserContext from '../../UserContext';
+import { tr } from 'date-fns/locale';
 
 const LeftDropBox = (props) => {
 
@@ -9,14 +10,58 @@ const LeftDropBox = (props) => {
 
     const [isOpen, setIsOpen] = useState(false);
 
-    const options = ['گزینه 1', 'گزینه 2', 'گزینه 3'];
+    const [targetPrice, setTargetPrice] = useState()
+    const [purchaseQty, setPurchaseQty] = useState()
+    const [chooseTradingType, setChooseTradingType] = useState([
+        { id: 1, title: 'none' },
+        { id: 2, title: 'Buy' },
+        { id: 3, title: 'Sale' },
+    ])
+
+    const [orderDate, setOrderDate] = useState()
+
+    const [selectedOptionCoin, setSelectedOptionCoin] = useState('');
+    const [selectedOptionType, setSelectedOptionType] = useState('');
+    const [idCounter, setIdCounter] = useState(0);
+
+    const [allState, setAllState] = useState([])
+
+
 
     const toggleDropDown = () => {
         setIsOpen(!isOpen)
     }
 
+    const onclickSaveHandler = (event) => {
+        const newId = idCounter + 1;
+        const newData = {
+            coin: selectedOptionCoin,
+            targetPrice: targetPrice,
+            purchaseQty: purchaseQty,
+            orderType: selectedOptionType,
+            id: newId,
+        };
+        setAllState([...allState, newData]);
+
+        setIdCounter(newId);
+        event.preventDefault();
+    };
+
+    const handleChangeCoinSelected = (event) => {
+        setSelectedOptionCoin(event.target.value);
+    }
+    const handleChangeTypeSelected = (event) => {
+        setSelectedOptionType(event.target.value);
+    }
+
+    const orderDeleteHandler = (rowId) => {
+        const updatedData = allState.filter((item) => item.id !== rowId);
+        setAllState(updatedData);
+    }
+
 
     return (
+
         <div className={`dropdown-box ${isOpen ? 'open' : ''}`}>
             <div className="dropdown-header" onClick={toggleDropDown}>
                 <p>Insert your Order
@@ -27,12 +72,21 @@ const LeftDropBox = (props) => {
                 <div className="accordion-collapse">
                     <div className="accordion-body">
                         <div className="form-floating1">
-                            <label htmlFor="" placeholder='Choose your Icon'>Choose your Coin</label>
-                            <select className='form-select' name="" id="">
-                                <option value="none">none</option>
+                            <label className='choose-your-icon' htmlFor="" placeholder='Choose your Icon'>Choose your Coin</label>
+                            <select
+                                value={selectedOptionCoin}
+                                className='form-select'
+                                name="" id=""
+                                onChange={handleChangeCoinSelected}>
+                                <option className='coin-options' value="none">none</option>
                                 {user && user.map((item) => {
                                     return (
-                                        <option key={item.id} value={item.id}>{item.id}_{item.current_price}$ </option>
+                                        <option
+                                            className='coin-options'
+                                            key={item.id}
+                                            value={item.id}>
+                                            {item.id}_{item.current_price}$
+                                        </option>
                                     )
                                 })}
                             </select>
@@ -40,24 +94,91 @@ const LeftDropBox = (props) => {
                         <form action="">
                             <div className='form-container'>
                                 <label htmlFor="" className='form-label'>Target Price</label>
-                                <input type="text" className='input-form-control' />
-                                <label htmlFor="" className='form-label'>Purchase quantity</label>
-                                <input type="text" className='input-form-control' />
+                                <input
+                                    type="number"
+                                    className='input-form-control'
+                                    id='target-price'
+                                    placeholder='0'
+                                    onChange={(e) => { setTargetPrice(e.target.value) }} />
+                                <label
+                                    htmlFor=""
+                                    className='form-label'>Purchase quantity
+                                </label>
+                                <input
+                                    type="number"
+                                    className='input-form-control'
+                                    placeholder='0'
+                                    onChange={(e) => { setPurchaseQty(e.target.value) }}
+                                />
                                 <div className="form-floating2">
                                     <label htmlFor="">Choose your Coin</label>
-                                    <select className='form-select' name="" id=""></select>
+                                    <select
+                                        className='form-select'
+                                        name="" id=""
+                                        onChange={handleChangeTypeSelected}
+                                        value={selectedOptionType}
+                                    >
+                                        {chooseTradingType.map((item) => {
+                                            return (
+                                                <option
+                                                    className='coin-options'
+                                                    key={item.id}
+                                                    value={item.title}>
+                                                    {item.title}
+                                                </option>
+                                            )
+                                        })}
+                                    </select>
                                 </div>
                                 <label htmlFor="" className='form-label'>Order Date</label>
-                                <input type="text" className='input-form-control' />
+                                <input
+                                    type="text"
+                                    className='input-form-control'
+                                    placeholder='dd-mm-yyyy'
+                                    onChange={(e) => { setOrderDate(e.target.value) }}
+                                />
 
                             </div>
-                            <button className='save-btn' >Save</button>
+                            <button className='save-btn' onClick={onclickSaveHandler} >Save</button>
 
                         </form>
                     </div>
                 </div>
             )}
+            <div className="order-table-container">
+                <table className='order-table'>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>C Name</th>
+                            <th>quantity</th>
+                            <th>T Price</th>
+                            <th>O Type</th>
+                            <th>Trash</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {allState.map((item) => {
+                            return <tr key={item.id} >
+                                <td>{item.id} </td>
+                                <td>{item.coin} </td>
+                                <td>{item.targetPrice} </td>
+                                <td>{item.purchaseQty} </td>
+                                <td>{item.orderType} </td>
+                                <td> <svg className='trash-svg2' onClick={() => orderDeleteHandler(item.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                </svg>
+                                </td>
+                            </tr>
+                        })}
+
+
+                    </tbody>
+                </table>
+            </div>
         </div>
+
+
     );
 };
 
