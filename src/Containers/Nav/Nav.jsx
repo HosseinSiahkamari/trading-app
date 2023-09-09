e9a5780f95cfa9082ea6e52a9ea38c86c37ca822
Mohammadhossein <siahkamarii1363@gmail.com>
2023-09-09 21:10:14 +0200

    added end

--- src/Containers/Nav/Nav.jsx
+++ src/Containers/Nav/Nav.jsx
@@ -17,48 +17,35 @@ const Nav = (props) => {
         }
     }, [props.dataFromChild]);
 
-    console.log(myState);
 
-
-    return (<div>
+    return (
         <div className="nav-div">
-            <input type="checkbox" id='check' />
-            <label htmlFor="check" className='check-btn'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                </svg>
-
-            </label>
             <nav className="nav-container">
-                <div className="button-container">
-                    <span className="navbar-brand">Trading App</span>
-
-                    <div className="main-btn-container">
-                        <div className="btn-container">
-                            <div className='add-order-container' >
-                                <button className='add-order' onClick={props.onClickLeftHandler}>Add Order </button>
-                                <div className="value-container">{myState.length} </div>
-                            </div>
-                            <div className="profolio-container">
-                                <button className='portfolio' onClick={props.onClickRightHandler}>portfolio management</button>
-                                <div className="value2-container">{profolioState.length} </div>
-                            </div>
-                            <button className='market-map'>Market Map</button>
-                            <button className='about-the-app' onClick={props.onClickCenterHandler}>About the App</button>
-                            <div className={`${result >= 0 ? 'green' : 'red'}`}>
-                                <span  > Profit or Loss{result}$ </span>
-                            </div>
-                        </div>
+                <input type="checkbox" id='check' />
+                <label htmlFor="check" className='check-btn'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </label>
+                <span className="navbar-brand">Trading App</span>
+                <div className="main-btn-container">
+                    <div className='add-order-container' >
+                        <button className='add-order' onClick={props.onClickLeftHandler}>Add Order </button>
+                        <div className="value-container">{myState.length} </div>
                     </div>
-
-
-
-
-
+                    <div className="profolio-container">
+                        <button className='portfolio' onClick={props.onClickRightHandler}>portfolio management</button>
+                        <div className="value2-container">{profolioState.length} </div>
+                    </div>
+                    <button className='market-map'>Market Map</button>
+                    <button className='about-the-app' onClick={props.onClickCenterHandler}>About the App</button>
+                    <div className={`${result >= 0 ? 'green' : 'red'}`}>
+                        <span  > Profit or Loss{result}$ </span>
+                    </div>
                 </div>
             </nav>
         </div>
-    </div>
+
     );
 }
 
--- src/setupProxy.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-const app = express();
-
-app.use('/api', createProxyMiddleware({ target: 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en', changeOrigin: true }));
-app.listen(3000);
